76ba5edd86afcb9aadb600dd5a44daf6a7887856
hajin Kim <102797869+haajinkim@users.noreply.github.com>
2022-07-08 17:47:41 +0900

    Update permissions.py

--- ta/permissions.py
+++ ta/permissions.py
@@ -10,7 +10,7 @@ class IsCandidateUser(BasePermission):
     message = 'candy 만 사용 할 수 있습니다.'
     
     def has_permission(self, request, view):
-        return bool(request.user and request.user.user_type=="candidate")
+        return bool(request.user and request.user.user_type.user_type=="candidate")
 
 class login_permission(BasePermission):
     def has_permission(self, request, view):
